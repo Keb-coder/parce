c933701c6dce22c8a6c3357fff52043e64565cdf
Cedric DAVASSOU <davassou.cedric.france@gmail.com>
2024-10-28 16:50:53 +0100

    Boutton ajouté

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import BoutonCed from './bouttonCed';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const tableau =[
+{   id:1,
+    description: "C'est l'histoire du ptit dej, tu la connais ? Pas de Bol",
+  },
+{
+  id:2,
+    description: "LA blague du soir ",
+},
+{
+  id:3,  
+    description: "C'est quoi un petit pois avec une épée face à une carotte avec une épée ? Un bon duel",
+},
+{
+    id:4,
+    description: "La blague ",
+},
+{
+    id:5,
+    description: "La",
+},
+
+  ];
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <header className='header'>
+      <h1 className='title'>Les blagues des Wild-Parces</h1>
+      <h3>Rigole avec nous</h3>
+      <span>choisis ton type de blague</span>
+      </header>
+      <main>
+        <section className="section">
+    <div className='classtest'>
+      <BoutonCed tableau={tableau} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BoutonCed tableau={tableau} />
+      <BoutonCed tableau={tableau} />
+      <BoutonCed tableau={tableau} />
+    
+    </section>
+    </main>
+    <footer>
+      <p>description</p>
+    </footer>
     </>
   )
 }
