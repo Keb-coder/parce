bfe4e7aa302a57c5ed58c0f7547345a1ae36b793
Cedric DAVASSOU <davassou.cedric.france@gmail.com>
2024-10-28 10:13:03 +0100

    Main.tsx > Ajout condition root, tsconfig.node.json > Ajout Linting

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+const rootElement = document.getElementById("root");
+
+
+if (rootElement == null) {
+
+  throw new Error(`Your HTML Document should contain a <div id="root"></div>`);
+
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
