bd94d3fc5f24c9c4b5250c81f67fd5583c683348
Cedric DAVASSOU <davassou.cedric.france@gmail.com>
2024-10-29 10:25:27 +0100

    Ajout étoile like

--- src/bouttonCed.tsx
+++ src/bouttonCed.tsx
@@ -10,6 +10,11 @@ interface valeur{
 function BoutonCed ({ tableau} : valeur ) {
 
 const [blagues, setBlagues] = useState("Afficher votre blague ici")    
+const [isLiked, setIsLiked] = useState(false);
+
+const toggleLike = ()=>{
+    setIsLiked(!isLiked);
+};
 
 const handleClick = () => {
 const random = Math.floor(Math.random() * tableau.length)
@@ -20,7 +25,7 @@ return(
 <div className="name">
   
     <button onClick={handleClick}>Cliquez-ici</button>
-    <p className="text"> <strong>{blagues}</strong></p>
+    <p className="text"> <strong>{blagues}</strong></p> <button className="star" onClick={toggleLike}>{isLiked ? "⭐" : "☆"}</button>
        
 </div>
 );
