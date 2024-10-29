f12f71558e6c80b499c8db58507659c440da462c
Ganburel Darhansukh <g.darhansukh@gmail.com>
2024-10-29 09:49:54 +0100

    app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Alcoolisme from "./alcoolisme";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [blagueAlcool, setBlagueAlcool] = useState(0);
+  const blaguesAlcoolisme = [
+    "Un fils demande à son pére: -Papa! C'est quoi un alcoolique? -Hé bien un  alcoolique, c'est quelqu'un qui va voir les choses en double. Tu vois par exemple ces 4 arbres là bas? L'alcoolique lui en verra 8. -Mais papa, il n'y a que 2 arbres",
+    "bl",
+    "vb",
+  ];
   return (
     <>
       <Alcoolisme
         blaguesAlcoolisme={blaguesAlcoolisme}
-     
+        blagueAlcool={blagueAlcool}
+        setBlagueAlcool={setBlagueAlcool}
       />
     </>
-  )
+  );
 }
-
-export default App
+export default App;
