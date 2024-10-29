185e9dd2df4380a44ae947e0c70b6b6e088550ae
ALEXANDRE JANASIEWICZ <alexandrejanasiewicz@gmail.com>
2024-10-29 10:30:20 +0100

    ajout des blagues

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,116 @@
-import './App.css'
-import BoutonCed from './bouttonCed';
+import "./App.css";
+import BoutonCed from "./bouttonCed";
 
 function App() {
+  const tableauCed = [
+    {
+      description: "C'est l'histoire du ptit dej, tu la connais ? Pas de Bol",
+    },
+    {
+      description: "La blague du soir ",
+    },
+    {
+      description:
+        "C'est quoi un petit pois avec une épée face à une carotte avec une épée ? Un bon duel",
+    },
+    {
+      description: "La blague ",
+    },
+    {
+      description: "La",
+    },
+  ];
 
-  const tableau =[
-{   id:1,
-    description: "C'est l'histoire du ptit dej, tu la connais ? Pas de Bol",
-  },
-{
-  id:2,
-    description: "LA blague du soir ",
-},
-{
-  id:3,  
-    description: "C'est quoi un petit pois avec une épée face à une carotte avec une épée ? Un bon duel",
-},
-{
-    id:4,
-    description: "La blague ",
-},
-{
-    id:5,
-    description: "La",
-},
+  const tableauAngelica = [
+    {
+      description:
+        "Tu ne serais pas une sortie de secours ? Parce que tu m’exit.",
+    },
+    {
+      description: "J’ai un problème avec mon portable : il manque ton numéro.",
+    },
+    {
+      description:
+        "Ton père il ne serait pas dealer de weed ? Parce que t’es sacrément bien roulée.",
+    },
+    {
+      description:
+        "Tu ne serais pas l’intro Netflix ? Parce que j’ai trop envie de te sauter!",
+    },
+    {
+      description:
+        "Les gens m’appellent Angélica, mais toi tu peux m’appeler ce soir.",
+    },
+  ];
 
+  const tableauAlex = [
+    {
+      description:
+        "Le père de Toto regarde son carnet de notes: - Tu sais quel est le point commun entre ton carnet de notes et un verre de limonade ? Lui demande t-il -Euh ... non. - Les bulles ! ",
+    },
+    {
+      description:
+        "C'est l'histoire d'un pingouin qui respire par les fesses, un jour il s'est assis et il est mort !",
+    },
+    {
+      description:
+        "La maman de Toto regarde le cahier de devoir de son fils :- Qu'est-ce que tu écris mal ! Tu ne pourrais pas écrire plus gros ? - Ah non. Quand j’écris trop gros, la maîtresse voit toutes les fautes",
+    },
+    {
+      description:
+        "Toto rentre de l’école en courant et se réfugie dans la salle de bain. Sa mère lui demande : Pourquoi tu vas au bain, Toto ? - Parce que la maîtresse m’a passé un savon !",
+    },
+    {
+      description:
+        "Toto va à la boulangerie. Le boulanger dit à Toto :- Aujourd’hui, le deuxième pain au chocolat est offert! - Alors, donnez-moi juste le deuxième.",
+    },
+  ];
+
+  const tableauDarkhaa = [
+    {
+      description:
+        "Un fils demande à son pére: -Papa! C'est quoi un alcoolique? -Hé bien un  alcoolique, c'est quelqu'un qui va voir les choses en double. Tu vois par exemple ces 4 arbres là bas? L'alcoolique lui en verra 8. -Mais papa, il n'y a que 2 arbres",
+    },
+    {
+      description:
+        "C'est un gars qui entre dans bar en marchant sur les mains. Il réussit à s'assoir péniblement et commande une bière. Ce dernier lui demande :10 h 17 -pourquoi marchez-vous sur les mains? -C'est ma femme , elle m'a dit de ne plus poser un pied dans un bar!",
+    },
+    {
+      description:
+        "Hier soir, j'étais tellement bourré que quand j'ai traversé la piste de danse pour aller aux toilettes, j'ai gagné le concours de danse.",
+    },
+    {
+      description:
+        "Quelle est la différence entre la bière et l'urine ? Environ un quart d'heure. ",
+    },
+    {
+      description:
+        "Chérie, je bois un verre avec mes potes. Si je ne suis pas là dans 30 minutes, RELIS CE MESSAGE !",
+    },
   ];
 
   return (
     <>
-    <header className='header'>
-      <h1 className='title'>Les blagues des Wild-Parces</h1>
-      <h3>Rigole avec nous</h3>
-      <span>choisis ton type de blague</span>
+      <header className="header">
+        <h1 className="title">Les blagues des Wild-Parces</h1>
+        <h3>Rigole avec nous</h3>
+        <span>choisis ton type de blague</span>
       </header>
       <main>
         <section className="section">
-    <div className='classtest'>
-      <BoutonCed tableau={tableau} />
-      </div>
-      <BoutonCed tableau={tableau} />
-      <BoutonCed tableau={tableau} />
-      <BoutonCed tableau={tableau} />
-    
-    </section>
-    </main>
-    <footer>
-      <p>description</p>
-    </footer>
+          <div className="classtest">
+            <BoutonCed tableau={tableauCed} />
+          </div>
+          <BoutonCed tableau={tableauAngelica} />
+          <BoutonCed tableau={tableauAlex} />
+          <BoutonCed tableau={tableauDarkhaa} />
+        </section>
+      </main>
+      <footer>
+        <p>description</p>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/bouttonCed.tsx
+++ src/bouttonCed.tsx
@@ -1,30 +1,29 @@
 import { useState } from "react";
 
-
-interface valeur{
-    tableau: { 
-        description: string,  
-        id: number }[];
+interface valeur {
+  tableau: {
+    description: string;
+  }[];
 }
 
-function BoutonCed ({ tableau} : valeur ) {
+function BoutonCed({ tableau }: valeur) {
+  const [blagues, setBlagues] = useState("Afficher votre blague ici");
 
-const [blagues, setBlagues] = useState("Afficher votre blague ici")    
+  const handleClick = () => {
+    const random = Math.floor(Math.random() * tableau.length);
+    setBlagues(tableau[random].description);
+  };
 
-const handleClick = () => {
-const random = Math.floor(Math.random() * tableau.length)
-    setBlagues (tableau[random].description);
-}
-
-return(
-<div className="name">
-  
-    <button onClick={handleClick}>Cliquez-ici</button>
-    <p className="text"> <strong>{blagues}</strong></p>
-       
-</div>
-);
-
+  return (
+    <div className="name">
+      <button type="button" onClick={handleClick}>
+        Cliquez-ici
+      </button>
+      <p className="text">
+        <strong>{blagues}</strong>
+      </p>
+    </div>
+  );
 }
 
 export default BoutonCed;
